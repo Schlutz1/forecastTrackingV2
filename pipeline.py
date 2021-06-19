1bd9b0c983bdbd84061079a6d8adcd0bf1102595
Max Schultz <MSchultz@quantium.com>
2021-06-19 23:23:01 +1000

    simpler structure

--- pipeline.py
+++ pipeline.py
@@ -13,10 +13,11 @@ import os
 
 # globals
 _id = 'pipeline.py'
-load_from_cache = True # use this for testing
+load_from_cache = False # use this for testing
 databaseHandler = DatabaseHandler()
 
 
+# define endpoints hit in pipeline
 pipeline_config = {
     "thredbo": {
         "handler": ThredboHandler(),
@@ -28,39 +29,56 @@ pipeline_config = {
     }
 }
 
+# iterate over endpoints
+for endpoint, config in pipeline_config.items():
 
-# for endpoint, config in pipeline_config.items():
+    # define handler
+    handler = config['handler']
 
-#     # define handler
-#     handler = config['handler']
+    # scrape soup
+    forecast_soup = handler.getForecast(load_from_cache)
+    
+    # parse soup into dataframe
+    df_parsed_forecast = handler.parseForecast(forecast_soup)
+    
+    # clean dataframe
+    df_cleaned_forecast = handler.cleanForecast(df_parsed_forecast)
 
-### Thredbo forecast interactions
-thredboHandler = ThredboHandler()
+    # store locally
+    databaseHandler.appendForecastData(
+        df_cleaned_forecast,
+        config['table']
+    )
 
-# get current forecast data, optionally load from cache
-forecast_soup = thredboHandler.getForecast(load_from_cache)
-df_parsed_forecast = thredboHandler.parseForecast(forecast_soup)
-df_cleaned_forecast = thredboHandler.cleanForecast(df_parsed_forecast)
+
+
+# ### Thredbo forecast interactions
+# thredboHandler = ThredboHandler()
+
+# # get current forecast data, optionally load from cache
+# forecast_soup = thredboHandler.getForecast(load_from_cache)
+# df_parsed_forecast = thredboHandler.parseForecast(forecast_soup)
+# df_cleaned_forecast = thredboHandler.cleanForecast(df_parsed_forecast)
+
+# # # store locally
+# databaseHandler.appendForecastData(
+#     df_cleaned_forecast,
+#     'thredbo'
+# )
+
+
+# ### Perisher forecast interactions
+# perisherHandler = PerisherHandler()
+
+# forecast_soup = perisherHandler.getForecast(load_from_cache)
+# df_parsed_forecast = perisherHandler.parseForecast(forecast_soup)
+# df_cleaned_forecast = perisherHandler.cleanForecast(df_parsed_forecast)
 
 # # store locally
-databaseHandler.appendForecastData(
-    df_cleaned_forecast,
-    'thredbo'
-)
-
-
-### Perisher forecast interactions
-perisherHandler = PerisherHandler()
-
-forecast_soup = perisherHandler.getForecast(load_from_cache)
-df_parsed_forecast = perisherHandler.parseForecast(forecast_soup)
-df_cleaned_forecast = perisherHandler.cleanForecast(df_parsed_forecast)
-
-# store locally
-databaseHandler.appendForecastData(
-    df_cleaned_forecast,
-    'perisher'
-)
+# databaseHandler.appendForecastData(
+#     df_cleaned_forecast,
+#     'perisher'
+# )
 
 
 ### Generate final export
