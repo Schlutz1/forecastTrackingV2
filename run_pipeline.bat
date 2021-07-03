581112ba431209f26f60fc58f778905baeba5137
Max Schultz <MSchultz@quantium.com>
2021-07-03 22:59:56 +1000

    data update

--- run_pipeline.bat
+++ run_pipeline.bat
@@ -1,2 +1,5 @@
 C:\Users\mschultz\AppData\Local\Microsoft\WindowsApps\python.exe "C:\Users\mschultz\Documents\99 Forecast Tracking\forecastTrackingV2\pipeline.py"
+git add .
+git commit -m "data update"
+git push origin master
 pause
